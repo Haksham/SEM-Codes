f4c670b240234f3ca5a0e74b2f18d8e9a1c08cad
Haksham <>
2024-06-25 06:56:15 +0530

    interval scheduling

---
+++ SEM-4/DAA/interval_scheduling.py
@@ -0,0 +1,12 @@
+list=[[90,91],[94,120],[80,90],[110,113],[150,190],[180,200]]
+list=sorted(list,key=lambda x:x[1])
+t=[]
+c=0
+p=0
+for i in list:
+  if p<i[0]:
+    t.append(i)
+    p=i[1]
+    c+=1
+print(t)
+print(c)
