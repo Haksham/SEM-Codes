537d2c5deb154ea4b55266d484b81c4e9123e370
Haksham <>
2024-06-28 08:14:07 +0530

    bellman ford added

---
+++ SEM-4/DAA/bellman_ford.py
@@ -0,0 +1,23 @@
+# G=[(3,2,6),(5,3,1),(0,1,5),(1,5,-3),(1,2,-2),(3,4,-2),(2,4,3)] # (u,v,weight) as directed graph(without -ve cycles)
+G=[(0,1,5),(1,2,1),(2,4,1),(1,3,2),(4,5,2),(4,3,-1),(5,4,-3)] # with -ve cycle
+inf=99999
+n=len(G)-1
+dist=[inf]*n
+
+root=0
+dist[root]=0
+
+
+def bellman(G,dist,n):
+  l=[]
+  for j in range(n):
+    for i in G:
+      if dist[i[0]]+i[2]<dist[i[1]]:
+        dist[i[1]]=dist[i[0]]+i[2]
+  print(dist)
+  for i in G:      
+    if dist[i[0]]+i[2]<dist[i[1]]:
+      print("-ve cycle detected!!")
+      break
+
+bellman(G,dist,n)
