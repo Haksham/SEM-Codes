fd90f9ef1fba92c8b13f13a47f3af02a14bd2cdc
HAKSHAM <harsh924hashvm@gmail.com>
2024-07-04 09:36:30 +0530

    Update weighted_interval_scheduling.py

--- SEM-4/DAA/weighted_interval_scheduling.py
+++ SEM-4/DAA/weighted_interval_scheduling.py
@@ -1,20 +1,18 @@
-list= [(1, 2, 50), (3, 5, 20),(6, 19, 100),(2, 10, 200)]  #(start, end , profit)
+#list= [(1, 2, 50), (3, 5, 20),(6, 19, 100),(2, 10, 200)]  #(start, end , profit)
 # list=[(1,4),(2,6),(5,8),(3,11),(9,15),(10,16)]
 # list=[(1,4,1),(2,6,3),(5,9,1)]
+list=[(1,2,100),(2,5,200),(3,6,300),(4,8,400),(5,9,500),(6,10,600)]
 
 list=sorted(list,key=lambda x:x[1])
 print(list)
 p=[0]*len(list)
 
 
-def GetP(i):
+for i in range(len(list)-1,0,-1):
   for j in range(i-1,-1,-1):
     if list[i][0]>=list[j][1]:
       p[i]=j+1
       break
-
-for i in range(len(list)-1,0,-1):
-  GetP(i)
 
 
 x=len(list)
