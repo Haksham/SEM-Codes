a4e293ed7394f3641aef48f4f2534b0fff1a9853
Haksham <harsh924hashvm@gmail.com>
2024-07-09 10:54:34 +0530

    added

--- Python-Lab(Test)/Q 8.py
+++ Python-Lab(Test)/Q 8.py
@@ -16,3 +16,26 @@ for i in ls:
 
 # b.	Develop a python program to create a text file and ask the user to enter 5-6 lines of text. Count the number of upper case, lower case and digits in the file. Display the details of the file.
 
+file=open("file.txt", "w")
+for i in range(5):
+  text=input("Enter text: ")
+  file.write(text+"\n")
+file.close()
+
+with open("file.txt", "r") as file:
+  text=file.read()
+  words=text.split()
+  Upper=0
+  lower=0
+  digits=0
+  for word in words:
+      for char in word:
+          if char.isupper():
+              Upper+=1
+          elif char.islower():
+              lower+=1
+          elif char.isdigit():
+              digits+=1
+  print("Number of uppercase letters:", Upper)
+  print("Number of lowercase letters:", lower)
+  print("Number of digits:", digits)
